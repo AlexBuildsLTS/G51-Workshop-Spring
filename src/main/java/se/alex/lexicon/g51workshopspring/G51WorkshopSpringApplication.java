81e1621fece0cac38b74d039282e2ebd0f5a0ae1
ALex Youssef <ALex.youssef@live.com>
2024-09-19 10:27:44 +0200

    Initial setup with Spring Boot, JPA, and database configurations. - Added dependencies: Spring Web, Spring Data JPA, MySQL Driver, and H2 Database, Lombak. - Configured MySQL for production and H2 for testing.

---
+++ src/main/java/se/alex/lexicon/g51workshopspring/G51WorkshopSpringApplication.java
@@ -0,0 +1,13 @@
+package se.alex.lexicon.g51workshopspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G51WorkshopSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G51WorkshopSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/alex/lexicon/g51workshopspring/G51WorkshopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package se.alex.lexicon.g51workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G51WorkshopSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
