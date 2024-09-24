b64d65fdad3968bbbaabeeab09c0a7784e08d8d6
ALex Youssef <ALex.youssef@live.com>
2024-09-24 12:57:50 +0200

    Added Address entity with fields and annotations for database mapping

--- src/main/java/se/alex/lexicon/g51workshopspring/entity/Address.java
+++ src/main/java/se/alex/lexicon/g51workshopspring/entity/Address.java
@@ -5,19 +5,22 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "Details")
-@Setter
+@Table(name = "Address")
 @Getter
+@Setter
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
-    private String email;
+    @Column(nullable = false)
+    private String street;
 
-    private String name;
+    @Column(nullable = false)
+    private String city;
 
-    @OneToOne(mappedBy = "details", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Student appUser;
+    @Column(nullable = false)
+    private String postalCode;
+
+    private String country;
 }
