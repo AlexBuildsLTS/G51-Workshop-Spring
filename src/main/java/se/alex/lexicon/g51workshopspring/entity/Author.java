02a33c35e75740bbb679e74278da7a37d5167864
ALex Youssef <ALex.youssef@live.com>
2024-09-26 18:03:54 +0200

    Created Author entity with properties and one-to-many relationship to Book

---
+++ src/main/java/se/alex/lexicon/g51workshopspring/entity/Author.java
@@ -0,0 +1,28 @@
+package se.alex.lexicon.g51workshopspring.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "Author")
+@Getter
+@Setter
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long authorId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Book> books;
+
+
+}
